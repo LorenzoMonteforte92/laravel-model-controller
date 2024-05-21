2825cd41e538736aa6ba2f99621942c118a0495e
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-21 16:12:09 +0200

    feat: add cinema card structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="container p-5">
         <div class="row p-5">
             <div class="col-12 d-flex justify-content-center align-items-center p-5">
-                <button type="button" class="btn btn-primary">Enter Cinema</button>
+                <a href="{{ route('cinema') }}" class="btn btn-primary">Enter Cinema</a>
             </div>
         </div>
     </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-12 text-center p-3">
-                <h1>CineLaravel</h1>
+            <div class="col-12 text-center text-white p-3">
+                <h1><a href="{{ route('home') }}">CineLaravel</a></h1>
             </div>
         </div>
     </div>
